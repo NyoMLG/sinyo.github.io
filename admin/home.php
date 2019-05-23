af6aec1ddbf1a71d052ae02ae174cd4fe2fa8e78
sinyo <phanzha.andy@gmail.com>
2019-05-24 04:56:45 +0700

    Revert "market"
    This reverts commit 5f0f34d4a8c3cbd26043e9d2921373cfa5cad0c1.

--- admin/home.php
+++
@@ -1,2 +0,0 @@
-<h2>selamat datang administrator</h2>
-<pre><?php print_r($_SESSION); ?></pre>
--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-session_destroy();
-echo "<script>alert('LOGOUT SUCCESS');</script>";
-echo "<script>location='login.php';</script>";
- ?>
